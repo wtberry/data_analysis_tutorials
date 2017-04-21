a1f43005d7646fefccc66629f13e3a8ffb105734
Chalmer Lowe <chalmer.lowe@gmail.com>
2017-04-21 01:57:25 -1000

    tweaks to demo scripts and server file

--- 01_wk/1_ip_and_city.py
+++ 01_wk/1_ip_and_city.py
@@ -8,6 +8,6 @@ for line in fin:
 
     # each field is stored in a list and indexed (starting at 0, 1, 2...)
     # compare the contents of the specific fields to specific values
-    if fields[0] == '218.69.80.64' and fields[5] == 'Wilmington':
+    if fields[0] == '31.175.105.171' and fields[5] == 'Portland':
         # clean up the line, by stripping newline characters off the right side
         print(line.rstrip())
