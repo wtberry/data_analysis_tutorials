94baf694782f3bab353a00287907826ff93226e7
Chalmer Lowe <chalmer.lowe@gmail.com>
2017-06-15 13:32:24 -1000

    added json_generator and updates to web_services_json.ipynb

--- 12/json_data_gen.py
+++ 12/json_data_gen.py
@@ -1,4 +1,7 @@
 import csv
+import json
+
+
 
 with open('AMEX_daily_prices_N.csv', encoding='utf-8-sig') as fin:
     reader = csv.reader(fin)
@@ -11,6 +14,26 @@ with open('AMEX_daily_prices_N.csv', encoding='utf-8-sig') as fin:
             header = line
             continue
         exchange, stock_sym, date, _open, high, low, close, volume, adj = line
-        data[stock_sym] = data.get(stock_sym, []) + [_open]
+        data[stock_sym] = data.get(stock_sym, []) + [high]
 
-print(data)
+print(len(data))
+
+with open('data.json', 'w') as fout:
+    json.dump(data, fout)
+
+
+with open('data.json') as inputfile:
+    prices = json.load(inputfile)
+
+    select_stock = {}
+    for stock in prices:
+        for price in prices[stock]:
+            num_price = float(price)
+            if num_price > 20.0:
+                select_stock[stock] = select_stock.get(stock, []) + [price]
+
+
+with open('prices.json', 'w') as outputfile:
+    json.dump(select_stock, outputfile)
+
+print(len(select_stock))
