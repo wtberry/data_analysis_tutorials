e6e92bcb1b662a1340b7ee6f69b0232c3d2e1e1b
Chalmer Lowe <chalmer.lowe@gmail.com>
2017-06-20 20:05:33 -1000

    fixed the alpha soln

--- practical/soln_assn_alpha.py
+++ practical/soln_assn_alpha.py
@@ -3,7 +3,7 @@ with open('113809of.fic') as fin:
     count = 1
     for word in fin.readlines():
         word = word.strip()
-        if word.count('a') == 2 and 'e' in word:
+        if word.count('a') == 2 and 'e' not in word:
             words.append(word)
             print(count, word)
             count += 1
